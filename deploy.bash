6d7925f027c811139d9b0fe99d6143ca7e78ae01
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-01-02 22:35:31 +0900

    fix

--- deploy.bash
+++ deploy.bash
@@ -4,9 +4,12 @@ set -euo pipefail
 
 base_dir=$(cd "$(dirname "$0")"; pwd)
 
-echo "check config..."
+echo "=== check config ==="
 promtool check config "$base_dir/chap02/systemd/prometheus.yml"
 
-echo "restart stack..."
+echo "=== reload systemctl ==="
+sudo systemctl daemon-reload
+echo "=== start prometheus stack ==="
 sudo systemctl restart prometheus-stack
 sudo systemctl status prometheus-stack | head -5
+echo "=== done ==="
